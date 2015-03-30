50f90fcc1b96909411662dc41f7cda33d725d784
caosiliang <siliang.cao@gmail.com>
2015-03-30 23:07:00 +0800

    lisp style code

--- putty_colors2crt.py
+++ putty_colors2crt.py
@@ -1,19 +1,41 @@
+from __future__ import print_function
+from itertools import tee, islice, izip
+
+
 color2pos = [8, 0, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15]
+line_size = 8
 
 ocolors = [ None ] * 16
 
 with open('E:\\colors.txt', 'r') as fp:
-    for l in fp:
-        num, carray, _ = l.split('\\')
-        num = int(num[6:])
+    r = [
+        (lambda num, array:(
+            int(num[6:]),
+            ['%02x' % int(e) for e in array.split(',')] + ['00']))
+        (
+            *((lambda t:(
+                t[0],
+                t[1]))
+            (l.split('\\'))))
+        for l in fp
+    ]
 
-        carray = carray.split(',')
-        c = ['%02x' % int(e) for e in carray]
-        c.append('00')
+    r = [ (color2pos[num-6], c) for num, c in r if num >= 6 ]
 
-        if num >= 6:
-            pos = color2pos[num-6]
-            ocolors[pos] = c
+    for pos, c in r:
+        ocolors[pos] = c
 
-for ostr in (ocolors[0:8], ocolors[8:16]):
-    print ' '.join(reduce(lambda x, y: x+y, ostr))
+    map(
+        print,
+        map(
+            lambda l:
+                ' '.join(
+                    reduce(
+                        lambda x, y: x+y,
+                        l)),
+            # split into line_size pieces
+            # there is also a grouper recipe: https://docs.python.org/2/library/itertools.html#recipes
+            izip(
+                *[islice(l, i, None, line_size) for i, l in
+                    enumerate(
+                        tee(ocolors, line_size))])))
