acacacd3fa2ca5e8ef297c9b8a200325f824b1ac
caosiliang <siliang.cao@gmail.com>
2015-05-10 17:31:44 +0800

    format update

--- putty_colors2crt.py
+++ putty_colors2crt.py
@@ -1,6 +1,6 @@
 color2pos = [8, 0, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15]
 
-ocolors = [ None ] * 16
+ocolors = [None] * 16
 
 with open('E:\\colors.txt', 'r') as fp:
     for l in fp:
@@ -12,8 +12,8 @@ with open('E:\\colors.txt', 'r') as fp:
         c.append('00')
 
         if num >= 6:
-            pos = color2pos[num-6]
+            pos = color2pos[num - 6]
             ocolors[pos] = c
 
 for ostr in (ocolors[0:8], ocolors[8:16]):
-    print ' '.join(reduce(lambda x, y: x+y, ostr))
+    print ' '.join(reduce(lambda x, y: x + y, ostr))
