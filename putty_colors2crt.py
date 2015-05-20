45ea9d9309c31edaba4e1098167ed74dc22e2ebf
caosiliang <siliang.cao@gmail.com>
2015-05-20 15:48:04 +0800

    Change to adapt to normal putty reg profile

--- putty_colors2crt.py
+++ putty_colors2crt.py
@@ -1,17 +1,19 @@
 from __future__ import print_function
 from itertools import izip_longest, imap
+import sys
 
 
 color2pos = [8, 0, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15]
 
-filename = '/home/caosiliang/colors.txt'
+
+filename = sys.argv[1] if len(sys.argv) >= 2 else 'E:\\terminal-sexy.txt'
 r = imap(
     lambda t: (
-        int(t[0][6:]),
-        ['%02x' % int(e) for e in t[1].split(',')] + ['00']
+        int(t[1][6:]),
+        ['%02x' % int(e) for e in t[3].split(',')] + ['00']
     ),
     imap(
-        lambda l: (l.split('\\')),
+        lambda l: (l.split('"')),
         open(filename, 'r')
     )
 )
