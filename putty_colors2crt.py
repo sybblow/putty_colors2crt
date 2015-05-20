2033a102fd86826f16f02269b478f52085d0f0a0
caosiliang <siliang.cao@gmail.com>
2015-05-20 16:22:00 +0800

    将导入做成可替换的逻辑

--- putty_colors2crt.py
+++ putty_colors2crt.py
@@ -6,35 +6,43 @@ import sys
 color2pos = [8, 0, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15]
 
 
-filename = sys.argv[1] if len(sys.argv) >= 2 else 'E:\\terminal-sexy.txt'
-r = imap(
-    lambda t: (
-        int(t[1][6:]),
-        ['%02x' % int(e) for e in t[3].split(',')] + ['00']
-    ),
-    ifilter(
-        lambda t: len(t) > 3,
+def putty_import(filename):
+    r = imap(
+        lambda t: (
+            int(t[1][6:]),
+            ['%02x' % int(e) for e in t[3].split(',')] + ['00']
+        ),
+        ifilter(
+            lambda t: len(t) > 3,
+            imap(
+                lambda l: (l.split('"')),
+                open(filename, 'r')
+            )
+        )
+    )
+    return [(num - 6, c) for num, c in r if num >= 6]
+
+
+def main():
+    filename = sys.argv[1] if len(sys.argv) >= 2 else 'E:\\terminal-sexy.txt'
+
+    output_colors = [None] * 16
+    for cnum, color in putty_import(filename):
+        output_colors[color2pos[cnum]] = color
+
+    group_size = 8
+    map(
+        print,
         imap(
-            lambda l: (l.split('"')),
-            open(filename, 'r')
+            lambda l: ' '.join(reduce(list.__add__, l)),
+            # a recipe take from "grouper" in this link:
+            # https://docs.python.org/2/library/itertools.html#recipes.
+            izip_longest(
+                *([iter(output_colors)] * group_size)
+            )
         )
     )
-)
-r = [(color2pos[num - 6], c) for num, c in r if num >= 6]
 
-output_colors = [None] * 16
-for pos, c in r:
-    output_colors[pos] = c
 
-group_size = 8
-map(
-    print,
-    imap(
-        lambda l: ' '.join(reduce(list.__add__, l)),
-        # a recipe take from "grouper" in this link:
-        # https://docs.python.org/2/library/itertools.html#recipes.
-        izip_longest(
-            *([iter(output_colors)] * group_size)
-        )
-    )
-)
+if __name__ == '__main__':
+    main()
