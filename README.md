7e0884da3332b4dc54df18273a9b95590a176e01
Siliang Cao <siliang.cao@gmail.com>
2021-05-18 22:37:38 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # putty_colors2crt
-A useful script to convert putty color scheme to SecureCRT's.
+A script to convert putty color scheme to SecureCRT's.
 
-为了方便别人拿去修改，我把啰嗦的容错逻辑都删了，尽可能保持清晰简洁。
+## Usage
+> python2 putty_colors2crt.py <colors.txt
 
-脚本生成的东西填到`%appdata%\VanDyke\Config\Global.ini`文件里的
-`B:"ANSI Color RGB"=00000040`下面，替换原有的两行就好了。
+paste the output to replace the 2 lines under `B:"ANSI Color RGB"=00000040` in file `%appdata%\VanDyke\Config\Global.ini`
--- putty_colors2crt.py
+++ putty_colors2crt.py
@@ -1,19 +1,20 @@
+import sys
+
 color2pos = [8, 0, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15]
 
 ocolors = [None] * 16
 
-with open('E:\\colors.txt', 'r') as fp:
-    for l in fp:
-        num, carray, _ = l.split('\\')
-        num = int(num[6:])
+for l in sys.stdin:
+    num, carray, _ = l.split('\\')
+    num = int(num[6:])
 
-        carray = carray.split(',')
-        c = ['%02x' % int(e) for e in carray]
-        c.append('00')
+    carray = carray.split(',')
+    c = ['%02x' % int(e) for e in carray]
+    c.append('00')
 
-        if num >= 6:
-            pos = color2pos[num - 6]
-            ocolors[pos] = c
+    if num >= 6:
+        pos = color2pos[num - 6]
+        ocolors[pos] = c
 
 for ostr in (ocolors[0:8], ocolors[8:16]):
     print ' '.join(reduce(lambda x, y: x + y, ostr))
